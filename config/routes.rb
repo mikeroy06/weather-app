3536594512a77e8512ac585251502cb617f3a941
Mike Roy <mikeroy06@bellsouth.net>
2017-10-11 08:36:06 -0400

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  
+  get 'test' => 'welcome#test'
+  post 'index' => 'welcome#index'
+  get 'index' => 'welcome#index'
+  root 'welcome#index'
+
+  
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
